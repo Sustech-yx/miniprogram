2999ee581e3e3b32cce8a4cd19947caa83d473db
Sustech-yx <59305237+Sustech-yx@users.noreply.github.com>
2022-04-04 20:35:34 +0800

    upd

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,10 +8,11 @@ Page({
     width_static : 0,
     height_static : 0,
     points_x : [],
-    points_y : []
+    points_y : [],
+    sliderValue:0
   },
   speed:function (e) {
-    console.log(`speed发生change事件，携带值为`, e.detail.value)
+    // console.log(`speed发生change事件，携带值为`, e.detail.value)
     let currentSpeed = e.detail.value.toFixed(3)
     this.setData({
       speed:currentSpeed
@@ -21,14 +22,24 @@ Page({
     // console.log(e)
     // console.log("x:" + e.touches[0].x)
     // console.log("y:" + e.touches[0].y)
-    var x = e.touches[0].x
-    var y = e.touches[0].y
+   
     var width = this.data.width_static
     var height = this.data.height_static
-    if ((y - height < -(x - width / 2)) && (height - y > -(x - width / 2))) {
-      this.data.points_x.push(x - width/2)
-      this.data.points_y.push(height - y)
-    }
+
+    var x = e.touches[0].x - width / 2
+    var y = e.touches[0].y - height / 2
+
+    this.data.points_x.push(x)
+    this.data.points_y.push(y)
+    
+  },
+  tapbutton:function(e) {
+    this.setData({
+      points_x : [],
+      points_y : [],
+      speed:0,
+      sliderValue:0
+    })
   },
   onLoad() {
     wx.createSelectorQuery()
@@ -52,7 +63,7 @@ Page({
         // console.log(width)
         // console.log(height)
         ctx.scale(dpr, dpr)
-        ctx.translate(width / 2, height)
+        ctx.translate(width / 2, height / 2)
         this.setData({
           width_static:width,
           height_static:height
@@ -60,23 +71,25 @@ Page({
         this.drawCoor(ctx, width, height)
 
         const renderLoop = () => {
-          this.render(canvas, ctx, width, height)
+          this.render(canvas, ctx)
           canvas.requestAnimationFrame(renderLoop)
+          this.drawCoor(ctx, width, height)
         }
         canvas.requestAnimationFrame(renderLoop)
       })
   },
   render(canvas, ctx, width, height) {
-    // ctx.clearRect(-3000, -3000, 3000, 3000)
+    ctx.clearRect(-1000, -1000, 10000, 10000)
+    this.drawCoor(ctx, width, height)
+    this.drawTra(ctx)
     this.drawOri(ctx)
-    this.drawTra(ctx, width, height)
   },
   drawOri(ctx) {
     var x, y
     for (const i in this.data.points_x) {
       x = this.data.points_x[i]
       y = this.data.points_y[i]
-      drawOriginPoint(ctx, x, -y)
+      drawOriginPoint(ctx, x, y)
     }
     function drawOriginPoint(ctx, x, y) {
       ctx.beginPath()
@@ -87,8 +100,18 @@ Page({
       ctx.stroke()
     }
   },
-  drawTra(ctx, width, height) {
+  drawTra(ctx) {
     // drawTranslatedPoint(ctx, -100, -100)
+    var v = this.data.speed/100
+    var gamma = 1/Math.sqrt(1-v*v)
+    var x, y, _x, _y
+    for (const i in this.data.points_x) {
+      x = this.data.points_x[i]
+      y = this.data.points_y[i]
+      _x = gamma * (x - v * y)
+      _y = gamma * (y - v * x)
+      drawTranslatedPoint(ctx, _x, _y)
+    }
     function drawTranslatedPoint(ctx, x, y) {
       ctx.beginPath()
       ctx.arc(x, y, 3, 0, Math.PI * 2)
@@ -99,28 +122,28 @@ Page({
     }
   },
   drawCoor(ctx, width, height) {
+    ctx.fillStyle = '#000000'
     ctx.fillText("x", width / 2 - 10, -10)
-    ctx.fillText("t", 10, - height + 10)
+    ctx.fillText("t", 10, - height / 2 + 10)
     // 绘制坐标轴信息
     ctx.lineWidth = 2
 
-    ctx.save()
     ctx.strokeStyle = 'yellow'
     ctx.beginPath()
-    ctx.moveTo(0, 0)
+    ctx.moveTo(-width / 2, width / 2)
     ctx.lineTo(width / 2, - width / 2)
-    ctx.moveTo(0, 0)
+    ctx.moveTo(width / 2, width / 2)
     ctx.lineTo(- width / 2, - width / 2)
     ctx.stroke()
-    ctx.restore()
 
+    ctx.strokeStyle = 'black'
     ctx.beginPath()
     ctx.moveTo(- width / 2, 0)
     ctx.lineTo(width / 2, 0)
     ctx.stroke()
 
     ctx.beginPath()
-    ctx.moveTo(0, 0)
+    ctx.moveTo(0, height)
     ctx.lineTo(0, - height)
     ctx.stroke()
   }
